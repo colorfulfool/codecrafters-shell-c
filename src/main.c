1cf66b9daae1c410740891aa20990f2a4cc76e46
Anton Khamets <anton.khamets@hey.com>
2026-05-23 20:28:47 +0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-  setbuf(stdout, NULL);
-
+void loop() {
   printf("$ ");
 
   char statement[256];
   scanf("%s", &statement);
 
   printf("%s: command not found\n", statement);
+  loop();
+}
+
+int main(int argc, char *argv[]) {
+  setbuf(stdout, NULL);
+
+  loop();
 
   return 0;
 }
