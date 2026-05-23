9112721a79140cf7597a8c3661ce07807b1a07de
Anton Khamets <anton.khamets@hey.com>
2026-05-23 20:35:50 +0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void loop() {
   printf("$ ");
@@ -7,14 +8,16 @@ void loop() {
   char statement[256];
   scanf("%s", &statement);
 
+  if (strcmp(statement, "exit") == 0) {
+    return;
+  }
+
   printf("%s: command not found\n", statement);
   loop();
 }
 
 int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
-
   loop();
-
   return 0;
 }
